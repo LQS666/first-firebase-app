ec00d6d5610e221822631a200e21511bfe952156
Paweł Turkot <p.turkot123@gmail.com>
2019-02-22 19:48:18 +0100

    Add creating new tasks

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const exercisesList = document.querySelector('#exercises-list');
+const form = document.querySelector('#add-exercise');
 
 // Create element and render cafe
 function renderExercise(doc) {
@@ -7,23 +8,43 @@ function renderExercise(doc) {
     let input = document.createElement("input");
     let name = document.createElement('span');
     let icon = document.createElement('span');
+    let deleteBtn = document.createElement('div');
+
 
     input.type = "checkbox";
     icon.classList.add('icon');
     name.classList.add('name');
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
+    deleteBtn.textContent = 'delete';
+    deleteBtn.classList.add('deleteBtn');
+
 
     label.appendChild(input);
     label.appendChild(icon);
     label.appendChild(name);
+    label.appendChild(deleteBtn);
     li.appendChild(label);
 
     exercisesList.appendChild(li);
+
+
+    deleteBtn.addEventListener('click', (e) => {
+        let id = e.target.parentElement.parentElement.getAttribute('data-id');
+        db.collection('exercises').doc(id).delete();
+    })
 }
 
-db.collection('exercises').get().then((snapshot) => {
+db.collection('exercises').orderBy('name').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderExercise(doc);
     })
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('exercises').add({
+        name: form.name.value
+    })
+    form.name.value = '';
 })
